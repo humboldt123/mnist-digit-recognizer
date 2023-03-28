f50d670b61128fb6a3ef487dc07b21221175bc07
vivi <38266782+humboldt123@users.noreply.github.com>
2023-03-28 17:00:18 -0400

    [feat] prediction

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # mnist-digit-recognizer
 
+## Why?
+This is the MNIST Digit Recognizer— the "Hello World" of Deep Learning. The purpose of this repo is for me to practice training a model.
+
 ## Requirements
 - `numpy`
 - `matplotlib`
 - `opencv-python`
 - `tensorflow`
+
+## Acknowledgement
+Thank you to NeuralNine's video on handwritten digit recognition! 🙏
--- main.py
+++ main.py
@@ -6,7 +6,21 @@ import os
 
 from train import train_model
 
+# import the model
 model_name = "mnist_digits"
 dataset = tensorflow.keras.datasets.mnist
-
 model = tensorflow.keras.models.load_model(model_name + ".model")
+
+i = 0
+while os.path.isfile(f"digits/{i}.png"):
+    try:
+        image = cv2.imread(f"digits/{i}.png")[:,:,0]
+        image = numpy.invert(numpy.array([image]))
+        prediction = model.predict(image)
+        print(f"Guessing {numpy.argmax(prediction)}")
+        pyplot.imshow(image[0], cmap=pyplot.cm.binary)
+        pyplot.show()
+    except:
+        print("Error")
+    finally:
+        i += 1
